160c727bc698de4277d27507a1f9e5eaf50e8a69
SachHoang <hoangsach2003@gmail.com>
2023-09-17 19:47:27 +0700

    Add project files.

---
+++ QuanlyNhahang/Giaodien.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace QuanlyNhahang
+{
+    partial class Giaodien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giaodien));
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnTK = new System.Windows.Forms.Button();
+            this.btnNL = new System.Windows.Forms.Button();
+            this.btnTD = new System.Windows.Forms.Button();
+            this.btnBA = new System.Windows.Forms.Button();
+            this.btnQLKH = new System.Windows.Forms.Button();
+            this.btnQLNV = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnMenu.Controls.Add(this.pictureBox1);
+            this.pnMenu.Controls.Add(this.btnLogout);
+            this.pnMenu.Controls.Add(this.btnTK);
+            this.pnMenu.Controls.Add(this.btnNL);
+            this.pnMenu.Controls.Add(this.btnTD);
+            this.pnMenu.Controls.Add(this.btnBA);
+            this.pnMenu.Controls.Add(this.btnQLKH);
+            this.pnMenu.Controls.Add(this.btnQLNV);
+            this.pnMenu.Location = new System.Drawing.Point(3, 3);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(212, 527);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnLogout.Location = new System.Drawing.Point(21, 472);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(162, 45);
+            this.btnLogout.TabIndex = 0;
+            this.btnLogout.Text = "Log out";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnTK
+            // 
+            this.btnTK.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnTK.Location = new System.Drawing.Point(21, 375);
+            this.btnTK.Name = "btnTK";
+            this.btnTK.Size = new System.Drawing.Size(162, 45);
+            this.btnTK.TabIndex = 0;
+            this.btnTK.Text = "Thống kê số liệu";
+            this.btnTK.UseVisualStyleBackColor = false;
+            // 
+            // btnNL
+            // 
+            this.btnNL.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnNL.Location = new System.Drawing.Point(21, 324);
+            this.btnNL.Name = "btnNL";
+            this.btnNL.Size = new System.Drawing.Size(162, 45);
+            this.btnNL.TabIndex = 0;
+            this.btnNL.Text = "QL Nguyên liệu";
+            this.btnNL.UseVisualStyleBackColor = false;
+            // 
+            // btnTD
+            // 
+            this.btnTD.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnTD.Location = new System.Drawing.Point(21, 273);
+            this.btnTD.Name = "btnTD";
+            this.btnTD.Size = new System.Drawing.Size(162, 45);
+            this.btnTD.TabIndex = 0;
+            this.btnTD.Text = "QL Thực đơn";
+            this.btnTD.UseVisualStyleBackColor = false;
+            // 
+            // btnBA
+            // 
+            this.btnBA.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnBA.Location = new System.Drawing.Point(21, 222);
+            this.btnBA.Name = "btnBA";
+            this.btnBA.Size = new System.Drawing.Size(162, 45);
+            this.btnBA.TabIndex = 0;
+            this.btnBA.Text = "QL Bàn ăn";
+            this.btnBA.UseVisualStyleBackColor = false;
+            this.btnBA.Click += new System.EventHandler(this.btnBA_Click);
+            // 
+            // btnQLKH
+            // 
+            this.btnQLKH.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnQLKH.Location = new System.Drawing.Point(21, 171);
+            this.btnQLKH.Name = "btnQLKH";
+            this.btnQLKH.Size = new System.Drawing.Size(162, 45);
+            this.btnQLKH.TabIndex = 0;
+            this.btnQLKH.Text = "QL Khách hàng";
+            this.btnQLKH.UseVisualStyleBackColor = false;
+            // 
+            // btnQLNV
+            // 
+            this.btnQLNV.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnQLNV.Location = new System.Drawing.Point(21, 120);
+            this.btnQLNV.Name = "btnQLNV";
+            this.btnQLNV.Size = new System.Drawing.Size(162, 45);
+            this.btnQLNV.TabIndex = 0;
+            this.btnQLNV.Text = "QL Nhân viên";
+            this.btnQLNV.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Giaodien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.pnMenu);
+            this.Name = "Giaodien";
+            this.Text = "Giaodien";
+            this.pnMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnTK;
+        private System.Windows.Forms.Button btnNL;
+        private System.Windows.Forms.Button btnTD;
+        private System.Windows.Forms.Button btnBA;
+        private System.Windows.Forms.Button btnQLKH;
+        private System.Windows.Forms.Button btnQLNV;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ QuanlyNhahang/Giaodien.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyNhahang
+{
+    public partial class Giaodien : Form
+    {
+        public Giaodien()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowLogin() //Viết 1 hàm không trả về giá trị và không đối số thực hiện việc hiển thị form 2
+
+        {
+            Login lg = new Login();
+            lg.ShowDialog();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(ShowLogin)); // Khởi tạo luồng mới
+            thread.Start();
+            this.Close();
+        }
+
+        private void btnBA_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
